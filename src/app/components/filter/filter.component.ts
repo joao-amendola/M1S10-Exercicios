0c530307f20f24f543bd190fea8440df79992bb3
João Amendola <mx.joaoo@gmail.com>
2022-12-12 11:45:19 -0300

    Tentativa de pegar a URL.

--- src/app/components/filter/filter.component.ts
+++ src/app/components/filter/filter.component.ts
@@ -14,18 +14,30 @@ export class FilterComponent implements OnInit {
   options = ['todos', 'nao-lidos', 'lidos']
   @Output() filtrarNotificacoes = new EventEmitter<string>()
   
+  // solucao 1: getter para pegar o parametro da URL
   public get filtroId(){
     return this.route.snapshot.paramMap.get('id')
   }
 
+  //solucao 2: 'id' no construtor e subscribe no OnInit
+  public id: string | null
+
   constructor(
     private router: Router,
     private route: ActivatedRoute,
-  ){}
+  ){
+    //solucao 2: 'id' no construtor e subscribe no OnInit
+    this.id = this.route.snapshot.paramMap.get('id')
+  }
   
   notificacoesFiltradas:INotificacao[] = []
   
   ngOnInit(): void {
+    //solucao 2: 'id' no construtor e subscribe no OnInit
+    this.route.paramMap.subscribe((data) => {
+      this.id = data.get('id')
+    })
+
     this.notificacoesFiltradas = NOTIFICATIONS_MOCK
   }
 
