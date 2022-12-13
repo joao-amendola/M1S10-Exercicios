b7a5052dfedddfe91af57acf818727204efe63f4
João Amendola <mx.joaoo@gmail.com>
2022-12-12 22:02:29 -0300

    Commit final, exercícios do M1S10 resolvidos

--- src/app/components/content/content.component.ts
+++ src/app/components/content/content.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { INotificacao } from 'src/app/models/notificacao.model';
+import { ActivatedRoute, Router } from "@angular/router";
 import { NOTIFICATIONS_MOCK } from 'src/app/utils/notifications-mock';
 
 @Component({
@@ -7,17 +8,37 @@ import { NOTIFICATIONS_MOCK } from 'src/app/utils/notifications-mock';
   templateUrl: './content.component.html',
   styleUrls: ['./content.component.scss'],
 })
+
 export class ContentComponent implements OnInit {
-  listaDeNotificacoes: INotificacao[] = [];
+  listaDeNotificacoes: INotificacao[] = NOTIFICATIONS_MOCK;
+  listaDeNotificacoesToRender: INotificacao[] = []
+  listFilter = ''
+  options = []
+  public id: string | null
+
+  constructor( private router: Router, private route: ActivatedRoute) {
+    this.id = this.route.snapshot.paramMap.get('id')
+  }
 
   ngOnInit(): void {
-    this.listaDeNotificacoes = NOTIFICATIONS_MOCK;
-  }
+    this.route.paramMap.subscribe((data) => {
+      this.id = data.get('id')
+
+    if(this.id == 'lidos'){
+      this.listaDeNotificacoesToRender = this.listaDeNotificacoes.filter((element) => element.lido == true)
+    }
+    else if (this.id == 'nao-lidos'){
+      this.listaDeNotificacoesToRender = this.listaDeNotificacoes.filter((element) => element.lido == false)
+    }
+    else {
+      this.listaDeNotificacoesToRender = this.listaDeNotificacoes      
+    }
+  })}
 
   lerNotificacao(indice: number) {
     console.log('indice', indice);
 
-    this.listaDeNotificacoes.forEach((item, index) => {
+    this.listaDeNotificacoesToRender.forEach((item, index) => {
       if (index === indice) {
         item.lido = true;
       }
--- src/app/components/filter/filter.component.ts
+++ src/app/components/filter/filter.component.ts
@@ -1,8 +1,6 @@
 import { Component, EventEmitter, Output, OnInit} from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { NOTIFICATIONS_MOCK } from 'src/app/utils/notifications-mock';
 import {INotificacao} from 'src/app/models/notificacao.model'
-import {routes} from 'src/app/app-routing.module'
 
 @Component({
   selector: 'ntap-filter',
@@ -13,49 +11,19 @@ import {routes} from 'src/app/app-routing.module'
 export class FilterComponent implements OnInit {
   options = ['todos', 'nao-lidos', 'lidos']
   @Output() filtrarNotificacoes = new EventEmitter<string>()
-  
-  // solucao 1: getter para pegar o parametro da URL
-  public get filtroId(){
-    return this.route.snapshot.paramMap.get('id')
-  }
-
-  //solucao 2: 'id' no construtor e subscribe no OnInit
-  public id: string | null
 
   constructor(
     private router: Router,
     private route: ActivatedRoute,
-  ){
-    //solucao 2: 'id' no construtor e subscribe no OnInit
-    this.id = this.route.snapshot.paramMap.get('id')
-  }
+  ){}
   
   notificacoesFiltradas:INotificacao[] = []
   
   ngOnInit(): void {
-    //solucao 2: 'id' no construtor e subscribe no OnInit
-    this.route.paramMap.subscribe((data) => {
-      this.id = data.get('id')
-    })
-    
-    this.notificacoesFiltradas = NOTIFICATIONS_MOCK
   }
 
   filtroSelecionado(event:any, filtroClicado:string) {
     this.filtrarNotificacoes.emit(filtroClicado)
     this.router.navigate(['/home', filtroClicado])
-    if (filtroClicado == "lidos"){
-      console.log("Lidos");
-      this.notificacoesFiltradas = this.notificacoesFiltradas.filter((element) => element.lido == true)
-      console.log(this.notificacoesFiltradas)
-    }
-    else if (filtroClicado == "nao-lidos"){
-      console.log("Não lidos");
-      
-    }
-    else {
-      console.log("Todos")
-    }
-
   }
 }
